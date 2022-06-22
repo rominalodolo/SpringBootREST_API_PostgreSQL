55efae51b4f99e0fc85895f311190e1ce02e82d9
Romina <lodoloromina@gmail.com>
2022-06-22 12:40:39 +0200

    update

--- expense-tracker-api/expensetracker_db.sql
+++ expense-tracker-api/expensetracker_db.sql
@@ -1,3 +1,10 @@
+drop database expensetrackerdb;
+drop user expensetracker;
+
 create user expensetracker with password 'password';
 create database expensetrackerdb with template=template0 owner=expensetracker;
+\connect expensetrackerdb;
+alter default privileges grant all on tables to expensetracker;
+all default privileges grant all on sequences to expensetracker;
+
 
