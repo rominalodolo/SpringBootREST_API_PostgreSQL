9c14a30886a930a095ca28c80f2a0904dffd5cf2
Romina <lodoloromina@gmail.com>
2022-06-23 10:34:53 +0200

    update

---
+++ expense-tracker-api/src/main/java/com/pairlearning/expensetrackerapi/exceptions/EtAuthException.java
@@ -0,0 +1,9 @@
+package com.pairlearning.expensetrackerapi.exceptions;
+
+public class EtAuthException extends  RuntimeException{
+
+    public EtAuthException(String message){
+        super(message);
+    }
+
+}
--- expense-tracker-api/src/main/java/com/pairlearning/expensetrackerapi/services/UserService.java
+++ expense-tracker-api/src/main/java/com/pairlearning/expensetrackerapi/services/UserService.java
@@ -1,4 +1,12 @@
 package com.pairlearning.expensetrackerapi.services;
 
+import com.pairlearning.expensetrackerapi.domain.User;
+
 public interface UserService {
+
+    User validateUser(String email, String password) throws EtAuthException;
+
+    User registerUser(String firstName, String  lastName, String  email, String password) throws EtAuthException;
+
+
 }
