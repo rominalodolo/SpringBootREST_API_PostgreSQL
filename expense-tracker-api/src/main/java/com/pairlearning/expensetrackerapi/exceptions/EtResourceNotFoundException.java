84d4d752b4ee619379726ee113ad5c7f79670908
Romina <lodoloromina@gmail.com>
2022-06-23 16:57:48 +0200

    update

--- expense-tracker-api/src/main/java/com/pairlearning/expensetrackerapi/exceptions/EtResourceNotFoundException.java
+++ expense-tracker-api/src/main/java/com/pairlearning/expensetrackerapi/exceptions/EtResourceNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class EtResourceNotFoundException {
+public class EtResourceNotFoundException extends RuntimeException {
     public EtResourceNotFoundException(String message) {
         super(message);
     }
---
+++ expense-tracker-api/src/main/java/com/pairlearning/expensetrackerapi/services/TransactionServiceImpl.java
@@ -0,0 +1,47 @@
+package com.pairlearning.expensetrackerapi.services;
+
+
+import com.pairlearning.expensetrackerapi.domain.Transaction;
+import com.pairlearning.expensetrackerapi.exceptions.EtBadRequestException;
+import com.pairlearning.expensetrackerapi.exceptions.EtResourceNotFoundException;
+import com.pairlearning.expensetrackerapi.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class TransactionServiceImpl implements TransactionService  {
+
+
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    @Override
+    public List<Transaction> fetchAllTransactions(Integer userId, Integer categoryId) {
+        return transactionRepository.findAll(userId, categoryId);
+    }
+
+    @Override
+    public Transaction fetchTransactionById(Integer userId, Integer categoryId, Integer transactionId) throws EtResourceNotFoundException {
+        return transactionRepository.findById(userId, categoryId, transactionId);
+    }
+
+    @Override
+    public Transaction addTransaction(Integer userId, Integer categoryId, Double amount, String note, Long transactionDate) throws EtBadRequestException {
+        int transactionId = transactionRepository.create(userId, categoryId, amount, note, transactionDate);
+        return transactionRepository.findById(userId, categoryId, transactionId);
+    }
+
+    @Override
+    public void updateTransaction(Integer userId, Integer categoryId, Integer transactionId, Transaction transaction) throws EtBadRequestException {
+        transactionRepository.update(userId, categoryId, transactionId, transaction);
+    }
+
+    @Override
+    public void removeTransaction(Integer userId, Integer categoryId, Integer transactionId) throws EtResourceNotFoundException {
+        transactionRepository.removeById(userId, categoryId, transactionId);
+    }
+}
